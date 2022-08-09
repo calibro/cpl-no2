91d0df22fac07c87d2b719ea0ea8a2fd8da55e8e
uf0 <scnepa222@gmail.com>
2022-08-09 18:35:45 +0200

    added action

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [preprocess()],
 	kit: {
 		adapter: adapter(),
+		prerender: {
+			default: true
+		},
+		paths: {
+			base: dev ? '' : '/cpl-no2'
+		}
 	}
 };
 
